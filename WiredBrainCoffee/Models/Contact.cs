66f171b6052423320f1364a3d2a785d327b3625a
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-30 13:35:24 +0300

    asp-validation-for

--- WiredBrainCoffee/Models/Contact.cs
+++ WiredBrainCoffee/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace WiredBrainCoffee.Models
 {
     public class Contact
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [Phone]
         public string Phone { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(10)]
         public string Message { get; set; }
     }
 }
